68c742d15163704016954a0f41be32b82512f2dd
mohak92 <tamhane.mohak@gmail.com>
2022-08-17 20:46:58 -0400

    initial commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mohak Tamhane on 8/17/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
