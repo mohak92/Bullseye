2a2408dfa40c59fe4d57a5a181cf58295755625d
mohak92 <tamhane.mohak@gmail.com>
2022-09-01 19:16:39 -0400

    game models

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,11 +8,32 @@
 import Foundation
 
 struct Game {
-    var target: Int = Int.random(in: 1 ... 100);
-    var score: Int = 0;
-    var round: Int = 1;
+    var target: Int = Int.random(in: 1 ... 100)
+    var score: Int = 0
+    var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        return 100 - abs(sliderValue - self.target)
+        let difference: Int = abs(sliderValue - self.target)
+        let bonus: Int
+        if difference == 0 {
+            bonus = 100
+        } else if difference <= 2 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        return 100 - difference + bonus
+    }
+    
+    mutating func startNewRound(points: Int) {
+        score += points
+        round += 1
+        target = Int.random(in: 1 ... 100)
+    }
+    
+    mutating func restart() {
+        score = 0
+        round = 1
+        target = Int.random(in: 1 ... 100)
     }
 }
