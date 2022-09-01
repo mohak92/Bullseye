8213674f90f48efae42f1a9499947577ef1317b7
mohak92 <tamhane.mohak@gmail.com>
2022-09-01 19:19:54 -0400

    updates and extract view to build reusable views

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -14,36 +14,80 @@ struct ContentView: View {
     @State private var game: Game = Game();
     
     var body: some View {
+        ZStack {
+           BackgroundView(game: $game)
+            VStack {
+                InstructionsView(game: $game)
+                    .padding(.bottom, 100)
+                HitMeButton(alertIsVisible: $alertIsVisible, sliderValue: $sliderValue, game: $game)
+            }
+            SliderView(sliderValue: $sliderValue)
+        }
+    }
+}
+
+struct InstructionsView: View {
+    
+    @Binding var game: Game
+    
+    var body: some View {
         VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+            InstructionText(text: "🎯🎯🎯\nPut the Bullseye as close as you can to")
+                .padding(.leading, 30.0)
+                .padding(.trailing, 30.0)
+            BigNumberText(text: String(game.target))
+        }
+    }
+}
+
+struct SliderView: View {
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        HStack {
+            SliderLabelText(text: "1")
+            Slider(value: self.$sliderValue, in: 1.0...100)
+            Text("100")
                 .bold()
-                .kerning(2.0)
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-            Text(String(game.target))
-                .kerning(-1.0)
-                .fontWeight(.black)
-                .font(.largeTitle)
-            HStack {
-                Text("1")
-                    .bold()
-                Slider(value: self.$sliderValue, in: 1.0...100)
-                Text("100")
-                    .bold()
+                .foregroundColor(Color("TextColor"))
+            
+        }
+        .padding()
+    }
+}
+
+struct HitMeButton: View {
+    
+    @Binding var alertIsVisible: Bool
+    @Binding var sliderValue: Double
+    @Binding var game: Game
+    
+    var body: some View {
+        Button(action: {
+            self.alertIsVisible = true;
+        }) {
+            Text("Hit me".uppercased())
+                .bold()
+        }
+        .padding(20.0)
+        .background(ZStack {
+            Color("ButtonColor")
+            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+        })
+        .foregroundColor(Color.white)
+        .cornerRadius(21.0)
+        .overlay(
+            RoundedRectangle(cornerRadius: 21.0)
+                .strokeBorder(Color.white, lineWidth: 2.0)
+        )
+        .alert("Hello there!", isPresented: $alertIsVisible) {
+            Button("Awesome") {
+                game.startNewRound(points: game.points(sliderValue: Int(sliderValue)))
             }
-            Button(action: {
-                self.alertIsVisible = true;
-            }) {
-                Text("Hit me")
-            }
-            .alert("Hello there!", isPresented: $alertIsVisible) {
-                Button("Awesome") {}
-            } message: {
-                let roundedValue: Int = Int(self.sliderValue.rounded())
-                Text("The slider's value is \(roundedValue).\n" +
-                     "You scored \(self.game.points(sliderValue: roundedValue)) points this round")
-            }
+        } message: {
+            let roundedValue: Int = Int(self.sliderValue.rounded())
+            Text("The slider's value is \(roundedValue).\n" +
+                 "You scored \(self.game.points(sliderValue: roundedValue)) points this round")
         }
     }
 }
@@ -51,8 +95,13 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.portrait)
         ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+            .previewInterfaceOrientation(.landscapeRight)
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.dark)
             .previewLayout(.fixed(width: 568, height: 320))
             .previewInterfaceOrientation(.landscapeRight)
     }
