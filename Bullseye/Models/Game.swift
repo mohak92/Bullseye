841db183fdc95280819239072882ca445d18f9d9
mohak92 <tamhane.mohak@gmail.com>
2022-09-03 19:16:00 -0400

    leaderboard struct, add entries to leaderboard and test data initializer

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -7,10 +7,26 @@
 
 import Foundation
 
+struct LeaderboardEntry {
+    var score: Int
+    var date: Date
+}
+
 struct Game {
     var target: Int = Int.random(in: 1 ... 100)
     var score: Int = 0
     var round: Int = 1
+    var leaderboardEntries: [LeaderboardEntry] = []
+    
+    init(loadTestData: Bool = false) {
+        if loadTestData {
+            leaderboardEntries.append(LeaderboardEntry(score: 100, date: Date()))
+            leaderboardEntries.append(LeaderboardEntry(score: 80, date: Date()))
+            leaderboardEntries.append(LeaderboardEntry(score: 200, date: Date()))
+            leaderboardEntries.append(LeaderboardEntry(score: 50, date: Date()))
+            leaderboardEntries.append(LeaderboardEntry(score: 20, date: Date()))
+        }
+    }
     
     func points(sliderValue: Int) -> Int {
         let difference: Int = abs(sliderValue - self.target)
@@ -29,11 +45,17 @@ struct Game {
         score += points
         round += 1
         target = Int.random(in: 1 ... 100)
+        addToLeaderboard(points: points)
     }
     
     mutating func restart() {
         score = 0
         round = 1
         target = Int.random(in: 1 ... 100)
+    }
+    
+    mutating func addToLeaderboard(points: Int) {
+        leaderboardEntries.append(LeaderboardEntry(score: points, date: Date()))
+        leaderboardEntries.sort{$0.score > $1.score}
     }
 }
